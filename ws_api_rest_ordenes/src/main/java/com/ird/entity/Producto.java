e4e2d50fee3cb58e1b28cba50db62f6e28cfcda4
iRD <ird.tech.developer@gmail.com>
2021-04-25 22:24:51 -0500

    feat: - Api-rest-ordenes - Refactorizando Entidad Producto con Anotaciones

--- ws_api_rest_ordenes/src/main/java/com/ird/entity/Producto.java
+++ ws_api_rest_ordenes/src/main/java/com/ird/entity/Producto.java
@@ -1,16 +1,34 @@
 package com.ird.entity;
 
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "PRODUCTOS")
 public class Producto {
-
+	
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "NOMBRE", nullable = false, length = 50)
 	private String nombre;
+	
+	@Column(name = "CATEGORIA", nullable = false, length = 100)
 	private String categoria;
+	
+	@Column(name = "PRECIO", nullable = false)
+	private Double precio;
 	
 }
