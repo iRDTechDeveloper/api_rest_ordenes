f068afe8c6fc0ba7c8b5972305efe23c73459013
iRD <ird.tech.developer@gmail.com>
2021-04-26 21:49:36 -0500

    feat: - Api-rest-ordenes - se crear clase services y se refactoriza la interfaz ProductoRepository, se crean metodos en la clase ProductoService, para posterior implementarlos en la interfaz

--- ws_api_rest_ordenes/src/main/java/com/ird/controllers/ProductoController.java
+++ ws_api_rest_ordenes/src/main/java/com/ird/controllers/ProductoController.java
@@ -1,6 +1,5 @@
 package com.ird.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,52 +14,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ird.entity.Producto;
-import com.ird.repository.ProductoRepository;
+import com.ird.services.ProductoService;
 
 @RestController
 public class ProductoController {
 	
 	@Autowired
-	private ProductoRepository prodRepo;
+	private ProductoService prodServices;
 	
 	@GetMapping(value = "producto/{productoId}")
 	public ResponseEntity<Producto> findProductoById(@PathVariable("productoId") Long productoId) {
-		Producto producto = prodRepo.findById(productoId)
-				.orElseThrow(() -> new RuntimeException("No Existe El Producto..."));
+		Producto producto = prodServices.findProductoById(productoId);
 		return new ResponseEntity<Producto>(producto, HttpStatus.OK);	
 	}
 	
 	@DeleteMapping(value = "deleteProducto/{productoId}")
 	public ResponseEntity<Void> deleteProducto(@PathVariable("productoId") Long productoId) {
-		Producto producto = prodRepo.findById(productoId)
-				.orElseThrow(() -> new RuntimeException("No Existe El Producto..."));
-		prodRepo.delete(producto);
-		return new ResponseEntity(producto, HttpStatus.OK);
+		prodServices.deleteProducto(productoId);
+		return new ResponseEntity(HttpStatus.OK);
 	}
 	
 	@GetMapping(value = "productos")
 	public ResponseEntity<List<Producto>> findAllProducto(){
-		List<Producto> listaProductos = prodRepo.findAll();
+		List<Producto> listaProductos = prodServices.findAllProducto();
 		return new ResponseEntity<List<Producto>>(listaProductos, HttpStatus.OK);
 	}
 	
 	@PostMapping(value = "/nuevoProducto")
 	public ResponseEntity<Producto> createProducto(@RequestBody Producto nuevoProducto) {
-		Producto crearNuevoProducto = prodRepo.save(nuevoProducto);
+		Producto crearNuevoProducto = prodServices.producto_Cr_Up(nuevoProducto);
 		return new ResponseEntity<Producto>(crearNuevoProducto, HttpStatus.CREATED);
 	}
 	
 	@PutMapping(value = "/updateProducto")
 	public ResponseEntity<Producto> updateProducto(@RequestBody Producto updateProducto) {
-		Producto productoExistente = prodRepo.findById(updateProducto.getId())
-				.orElseThrow(() -> new RuntimeException("No Existe El Producto..."));
-		
-		productoExistente.setNombre(updateProducto.getNombre());
-		productoExistente.setCategoria(updateProducto.getCategoria());
-		productoExistente.setPrecio(updateProducto.getPrecio());
-		
-		prodRepo.save(productoExistente);
-		
+		Producto productoExistente = prodServices.producto_Cr_Up(updateProducto);
 		return new ResponseEntity<Producto>(productoExistente, HttpStatus.OK);
 	}
 	
---
+++ ws_api_rest_ordenes/src/main/java/com/ird/services/ProductoService.java
@@ -0,0 +1,54 @@
+package com.ird.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.ird.entity.Producto;
+import com.ird.repository.ProductoRepository;
+
+@Service
+public class ProductoService {
+	
+	@Autowired
+	private ProductoRepository prodRepo;
+	
+	public List<Producto> findAllProducto(){
+		List<Producto> listaProductos = prodRepo.findAll();
+		return listaProductos;
+	}
+	
+	public Producto findProductoById(Long productoId) {
+		Producto producto = prodRepo.findById(productoId)
+				.orElseThrow(() -> new RuntimeException("No Existe El Producto..."));
+		return producto;	
+	}
+	
+	@Transactional
+	public void deleteProducto(Long productoId) {
+		Producto producto = prodRepo.findById(productoId)
+				.orElseThrow(() -> new RuntimeException("No Existe El Producto..."));
+		prodRepo.delete(producto);
+	}
+	
+	@Transactional
+	public Producto producto_Cr_Up(Producto productoCU) {
+		
+		if(productoCU.getId() == null) {
+			Producto crearNuevoProducto = prodRepo.save(productoCU);
+			return crearNuevoProducto;
+		}
+		
+		Producto productoExistente = prodRepo.findById(productoCU.getId())
+				.orElseThrow(() -> new RuntimeException("No Existe El Producto..."));
+		
+		productoExistente.setNombre(productoCU.getNombre());
+		productoExistente.setCategoria(productoCU.getCategoria());
+		productoExistente.setPrecio(productoCU.getPrecio());
+		
+		prodRepo.save(productoExistente);
+		
+		return productoExistente;	
+	}
+}
