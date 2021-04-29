55d56b5d983b89c424024fa92891d973f22f6237
iRD <ird.tech.developer@gmail.com>
2021-04-29 16:58:50 -0500

    feat: Estandarizando y controlando el manejo de errores y excepciones

--- ws_api_rest_ordenes/src/main/java/com/ird/controllers/ProductoController.java
+++ ws_api_rest_ordenes/src/main/java/com/ird/controllers/ProductoController.java
@@ -1,7 +1,6 @@
 package com.ird.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -21,64 +20,74 @@ import com.ird.DTO.ProductoDTO;
 import com.ird.converters.ProductoConverter;
 import com.ird.entity.Producto;
 import com.ird.services.ProductoService;
+import com.ird.utils.WrapperResponse;
 
 @RestController
 public class ProductoController {
-	
+
 	@Autowired
 	private ProductoService prodServices;
-	
+
 	private ProductoConverter prodConvert = new ProductoConverter();
-	
+
 	@GetMapping(value = "producto/{productoId}")
-	public ResponseEntity<ProductoDTO> findProductoById(@PathVariable("productoId") Long productoId) {
-		
+	public ResponseEntity<WrapperResponse<ProductoDTO>> findProductoById(@PathVariable("productoId") Long productoId) {
+
 		Producto producto = prodServices.findProductoById(productoId);
-		
+
 		ProductoDTO productoDTO = prodConvert.fromEntity(producto);
-		
-		return new ResponseEntity<ProductoDTO>(productoDTO, HttpStatus.OK);	
+
+		return new WrapperResponse<ProductoDTO>(true, "Successful", productoDTO).createResponse(HttpStatus.OK);
+
+		// WrapperResponse<ProductoDTO> wpResponseDTO = new WrapperResponse<ProductoDTO>(true, "Successful", productoDTO);
+		// return new ResponseEntity<WrapperResponse<ProductoDTO>>(wpResponseDTO, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping(value = "deleteProducto/{productoId}")
-	public ResponseEntity<Void> deleteProducto(@PathVariable("productoId") Long productoId) {
+	public ResponseEntity<?> deleteProducto(@PathVariable("productoId") Long productoId) {
 		prodServices.deleteProducto(productoId);
-		return new ResponseEntity(HttpStatus.OK);
+
+		return new WrapperResponse(true, "Successful", null).createResponse(HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = "productos")
 	public ResponseEntity<List<ProductoDTO>> findAllProducto(
 			@RequestParam(value = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-			@RequestParam(value = "pageSize", required = false, defaultValue = "5") int pageSize
-			){
-		
+			@RequestParam(value = "pageSize", required = false, defaultValue = "5") int pageSize) {
+
 		Pageable pagina = PageRequest.of(pageNumber, pageSize);
-		
+
 		List<Producto> listaProductos = prodServices.findAllProducto(pagina);
-		
+
 		List<ProductoDTO> listaProductosDTO = prodConvert.fromEntity(listaProductos);
-		
-		return new ResponseEntity<List<ProductoDTO>>(listaProductosDTO, HttpStatus.OK);
+
+		return new WrapperResponse(true, "Successful", listaProductosDTO).createResponse(HttpStatus.OK);
+
+		// return new ResponseEntity<List<ProductoDTO>>(listaProductosDTO, HttpStatus.OK);
 	}
-	
+
 	@PostMapping(value = "/nuevoProducto")
 	public ResponseEntity<ProductoDTO> createProducto(@RequestBody ProductoDTO nuevoProducto) {
-		
+
 		Producto crearNuevoProducto = prodServices.producto_Cr_Up(prodConvert.fromDTO(nuevoProducto));
-		
+
 		ProductoDTO productoDTO = prodConvert.fromEntity(crearNuevoProducto);
-		
-		return new ResponseEntity<ProductoDTO>(productoDTO, HttpStatus.CREATED);
+
+		return new WrapperResponse(true, "Successful", productoDTO).createResponse(HttpStatus.CREATED);
+
+		// return new ResponseEntity<ProductoDTO>(productoDTO, HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping(value = "/updateProducto")
 	public ResponseEntity<ProductoDTO> updateProducto(@RequestBody ProductoDTO updateProducto) {
-		
+
 		Producto productoExistente = prodServices.producto_Cr_Up(prodConvert.fromDTO(updateProducto));
-		
+
 		ProductoDTO productoDTO = prodConvert.fromEntity(productoExistente);
-		
-		return new ResponseEntity<ProductoDTO>(productoDTO, HttpStatus.OK);
+
+		return new WrapperResponse(true, "Successful", productoDTO).createResponse(HttpStatus.OK);
+
+		// return new ResponseEntity<ProductoDTO>(productoDTO, HttpStatus.OK);
 	}
-	
+
 }
--- ws_api_rest_ordenes/src/main/java/com/ird/validator/ProductoValidator.java
+++ ws_api_rest_ordenes/src/main/java/com/ird/validator/ProductoValidator.java
@@ -1,29 +1,30 @@
 package com.ird.validator;
 
 import com.ird.entity.Producto;
+import com.ird.exceptions.ValidateServiceException;
 
 public class ProductoValidator {
 
 	public static void validarInsert(Producto validarProducto) {
 		
 		if(validarProducto.getNombre() == null || validarProducto.getNombre().trim().isEmpty()) {
-			throw new RuntimeException("EL nombre es requerido!!! ...");
+			throw new ValidateServiceException("EL nombre es requerido!!! ...");
 		}
 		
 		if(validarProducto.getNombre().length() > 50 || validarProducto.getNombre().length() < 3) {
-			throw new RuntimeException("EL valor del nombre es incorrecto, (Min 3 Digitos)-(Max 50 Digitos)!!! ...");
+			throw new ValidateServiceException("EL valor del nombre es incorrecto, (Min 3 Digitos)-(Max 50 Digitos)!!! ...");
 		}
 		
 		if(validarProducto.getCategoria() == null || validarProducto.getCategoria().trim().isEmpty()) {
-			throw new RuntimeException("EL nombre de la categoria es requerido...");
+			throw new ValidateServiceException("EL nombre de la categoria es requerido...");
 		}
 		
 		if(validarProducto.getCategoria().length() > 100 || validarProducto.getCategoria().length() < 5) {
-			throw new RuntimeException("EL valor de la categoria es incorrecto, (Min 5 Digitos)-(Max 100 Digitos)!!! ...");
+			throw new ValidateServiceException("EL valor de la categoria es incorrecto, (Min 5 Digitos)-(Max 100 Digitos)!!! ...");
 		}
 		
 		if(validarProducto.getPrecio() == null || validarProducto.getPrecio() < 0) {
-			throw new RuntimeException("EL valor del precio es incorrecto, digite un valor correcto!!! ...");
+			throw new ValidateServiceException("EL valor del precio es incorrecto, digite un valor correcto!!! ...");
 		}
 	}
 }
